1a9e34fa83fa0629bc4ef0db2c902e7c443cce52
hacker <610173878@qq.com>
2023-04-28 11:42:35 +0800

    update

--- wxcloudrun/models.py
+++ wxcloudrun/models.py
@@ -1,17 +1,15 @@
-from datetime import datetime
-
 from django.db import models
 
-
-# Create your models here.
-class Counters(models.Model):
-    id = models.AutoField
-    count = models.IntegerField(max_length=11, default=0)
-    createdAt = models.DateTimeField(default=datetime.now(), )
-    updatedAt = models.DateTimeField(default=datetime.now(),)
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return self.title
+
 
     class Meta:
         db_table = 'Counters'  # 数据库表名
